c149dfb80502db1b08d8cb27df8e2584ef810bb3
Pfefan <stefanpfeiffer04@gmail.com>
2022-06-21 21:05:20 +0200

    fixed errors while downloading

--- main.py
+++ main.py
@@ -12,22 +12,23 @@ class Imagehandler():
         self.Url = "https://prnt.sc/"
         self.Stringlen = 6
         self.sorted_list = []
-        self.imagecount = 0
-        self.imagelimit = 0
 
     def main(self):
         """main func to call all functions"""
 
-        self.imagelimit = input("enter how many pictures you want: ")
-        while int(self.imagelimit) > self.imagecount:
+        imagecount = 0
+        imagelimit = input("enter how many pictures you want: ")
+
+        while int(imagelimit) > imagecount:
             self.urlget()
-            self.imagecount += 1
+            imagecount += 1
 
     def urlget(self):
         """funtion to get urls of pictures"""
 
         self.Url = "https://prnt.sc/"
         self.sorted_list.clear()
+
         ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = self.Stringlen))    #random 6 character long string
         self.Url = self.Url + str(ran)
 
@@ -40,7 +41,9 @@ class Imagehandler():
             out = str(i).split('"')
             self.sorted_list.append(out[1])
 
-        self.download(self.sorted_list[10])
+        if not len(self.sorted_list) < 10:
+            self.download(self.sorted_list[10])
+
 
 
     def download(self, imgurl):
@@ -48,12 +51,10 @@ class Imagehandler():
 
         if imgurl.split("/")[2] == "st.prntscr.com":
             imgurl = "http:" + imgurl
-        print(imgurl)
         filename = imgurl.split("/")[-1]
 
         response = requests.get(imgurl, headers={'User-Agent': 'Chrome'})
 
-        print(response.status_code)
         if response.status_code == 200:
             with open("screenshots/" + filename,'wb') as f:
                 f.write(response.content)
